241c762282f8985d80de88ca7d07fa0a023a6029
Mateusz Krysiak <mateuszkrysiak05@gmail.com>
2025-10-08 20:11:51 +0200

    adding DatesManager class

---
+++ src/main/java/kryszque/todoapp/dates/DatesManager.java
@@ -0,0 +1,19 @@
+package kryszque.todoapp.dates;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DatesManager {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static boolean checkDate(String date){
+
+        try{
+            LocalDate convertedDate = LocalDate.parse(date, formatter);
+            return !convertedDate.isBefore(LocalDate.now());
+        }catch(DateTimeParseException e){
+            return false;
+        }
+    }
+
+
+}
